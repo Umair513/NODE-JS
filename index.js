6a37207f20fc1afe181d0ba4875452beb385e4ef
Umair513 <soomroumair513@gmail.com>
2023-06-11 02:13:09 +0500

    Module Scope

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const add = require("./add")
-
-console.log("Hello from index.js")
-const sum = add(2, 8)
-const sum2 = add(7,9)
-console.log(sum)
-console.log(sum2)
+require("./batman")
+require("./superman")
