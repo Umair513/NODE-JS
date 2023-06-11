f2472069e1d21186952d2f96d8e9b99a6cd7152c
Umair513 <soomroumair513@gmail.com>
2023-06-12 01:24:24 +0500

    Path Buildin Module

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// const data = require("./data.json")
-// console.log(data.name)
+const path = require("path")
+// console.log(__filename)
+// console.log(__dirname)
 
-console.log("Hello World")
+// console.log(path.basename(__filename))
+// console.log(path.basename(__dirname))
+
+// console.log(path.extname(__filename))
+// console.log(path.extname(__dirname))
+
+// console.log(path.parse(__filename))
+// console.log(path.format(path.parse(__filename)))
+
+// console.log(path.isAbsolute(__filename))
+
+// console.log(path.join("folder1", "folder2", "index.html"))
+// console.log(path.join("folder1", "folder2", "../index.html"))
+// console.log(path.join(__dirname, "data.json"))
+
+console.log(path.resolve("folder1", "folder2", "index.html"))
+console.log(path.resolve("folder1", "folder2", "../index.html"))
+console.log(path.resolve(__dirname, "data.json"))
