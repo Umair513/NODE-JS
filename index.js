a6d4aa1b0595d354694d8cc4ab758e28c780f058
Umair513 <soomroumair513@gmail.com>
2023-06-13 01:10:59 +0500

    pipe

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const fs = require("fs")
+const zlib = require("zlib")
+
+const gzip = zlib.createGzip()
 
 const readableStream = fs.createReadStream("./file.txt", {
     encoding: "utf-8",
     highWaterMark: 2
 })
 
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"))
+
 const writableStream = fs.createWriteStream("./file2.txt")
 
-readableStream.on("data", (chunk) => {
-    console.log(chunk)
-    writableStream.write(chunk)
-})
+readableStream.pipe(writableStream)
+
+// readableStream.on("data", (chunk) => {
+//     console.log(chunk)
+//     writableStream.write(chunk)
+// })
