a93b403663f9be89c5b3a30e0b2653149f337ba7
Umair513 <soomroumair513@gmail.com>
2023-06-12 23:13:49 +0500

    Buffer

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-// const EventEmitter = require("events")
-
-// const emitter = new EventEmitter()
-// emitter.on("order-pizza", (size, topping) => {
-//     console.log(`Order Received, Baking ${size} Pizza with ${topping} `)
-// })
-
-// emitter.on("order-pizza", (size) => {
-//     if (size == "Large") {
-//         console.log("Serving Extra Complements")
-//     }
-// })
-// emitter.emit("order-pizza", "Large", "Mushroom")
 
 
-const PizzaShop = require("./pizza-shop")
-const DrinkMachine = require("./drink-machine")
-const drinkMachine = new DrinkMachine()
-
-const pizzaShop = new PizzaShop()
-
-pizzaShop.on("order", (size, topping) => {
-    console.log(`Order Received, Baking ${size} Pizza with ${topping} `)
-    drinkMachine.serveDrink(size)
-})
-pizzaShop.order("Large", "Tommato")
-pizzaShop.displayOrderNumber()
-
+const buffer = Buffer.from("Umair", "utf-8")
+buffer.write("Hello")
+console.log(buffer.toJSON())
+console.log(buffer.toString())
+console.log(buffer)
