4b4c8f5b5d5f0c633df4477ee52f8ab6280be7fa
Umair513 <soomroumair513@gmail.com>
2023-06-13 00:11:49 +0500

    fs module

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+const { error } = require("console")
+const fs = require("fs/promises")
 
 
-const buffer = Buffer.from("Umair", "utf-8")
-buffer.write("Hello")
-console.log(buffer.toJSON())
-console.log(buffer.toString())
-console.log(buffer)
+
+// fs.readFile("./file.txt", "utf-8")
+//     .then(data => console.log(data) )
+//     .catch(error => console.log(error))
+
+
+// const fs = require("fs")
+
+// const fileContents = fs.readFileSync("./file.txt", "utf-8")
+// console.log(fileContents)
+
+// fs.readFile("./file.txt", "utf-8", (error, data) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(data)
+//     }
+// })
+
+// fs.writeFileSync("./greet.txt", "Hello World" )
