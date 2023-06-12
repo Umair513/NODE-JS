6231abc4797555f38a5717fa80795b44904e5079
Umair513 <soomroumair513@gmail.com>
2023-06-13 01:00:10 +0500

    Streams

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const { error } = require("console")
-const fs = require("fs/promises")
+const fs = require("fs")
 
+const readableStream = fs.createReadStream("./file.txt", {
+    encoding: "utf-8",
+    highWaterMark: 2
+})
 
+const writableStream = fs.createWriteStream("./file2.txt")
 
-// fs.readFile("./file.txt", "utf-8")
-//     .then(data => console.log(data) )
-//     .catch(error => console.log(error))
-
-
-// const fs = require("fs")
-
-// const fileContents = fs.readFileSync("./file.txt", "utf-8")
-// console.log(fileContents)
-
-// fs.readFile("./file.txt", "utf-8", (error, data) => {
-//     if (error) {
-//         console.log(error)
-//     } else {
-//         console.log(data)
-//     }
-// })
-
-// fs.writeFileSync("./greet.txt", "Hello World" )
+readableStream.on("data", (chunk) => {
+    console.log(chunk)
+    writableStream.write(chunk)
+})
