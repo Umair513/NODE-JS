1b6380623756199404d0964ee101af54965e1d6b
Umair513 <soomroumair513@gmail.com>
2023-06-12 22:03:01 +0500

    Callback Pattern

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-const path = require("path")
-// console.log(__filename)
-// console.log(__dirname)
+function greet(name) {
+    console.log(`Hello ${name}`)
+}
 
-// console.log(path.basename(__filename))
-// console.log(path.basename(__dirname))
+function greetUmair(greetFn) {
+    const name = "Umair"
+    greetFn(name)
+}
 
-// console.log(path.extname(__filename))
-// console.log(path.extname(__dirname))
-
-// console.log(path.parse(__filename))
-// console.log(path.format(path.parse(__filename)))
-
-// console.log(path.isAbsolute(__filename))
-
-// console.log(path.join("folder1", "folder2", "index.html"))
-// console.log(path.join("folder1", "folder2", "../index.html"))
-// console.log(path.join(__dirname, "data.json"))
-
-console.log(path.resolve("folder1", "folder2", "index.html"))
-console.log(path.resolve("folder1", "folder2", "../index.html"))
-console.log(path.resolve(__dirname, "data.json"))
+greetUmair(greet)
