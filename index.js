38bb30931047412a4d59cf94173cea9dc6c3afcb
Umair513 <soomroumair513@gmail.com>
2023-06-13 18:58:34 +0500

    Sending JSON Response

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-type": "text/plain"})
-    res.end("Hello World")
+
+    const SuperHero = {
+        firstName: "Umair",
+        lastName: "Ahmed"
+    }
+    res.writeHead(200, {"content-type": "application/json"})
+    res.end(JSON.stringify(SuperHero))
 })
 
 server.listen(3000, () => {
